fcf8bcf8a0aeeaa6b54237711228da625ce10a23
Glitch (glittery-grand-magnesium) <none>
2022-04-16 00:37:06 +0000

    🏎💨 Checkpoint ./script.js:43918567/1122 ./index.html:43918567/206 ./style.css:43918567/265

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 var pattern = [2, 1, 2, 4, 3, 2, 4, 1];
 var progress = 0; 
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;  //must be between 0.0 and 1.0
 
 function startGame(){
   //initialize game variables
@@ -24,3 +26,44 @@ function stopGame(){
 }
 
 console.log("Hello, world!");
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 440,
+  2: 493.883,
+  3: 554.365,
+  4: 587.329
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext 
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
